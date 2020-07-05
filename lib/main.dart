2aa3e67857bbe307dc9822d56425248ed8cdc5ae
GeorgeYZJ <yangzhuojun1994@gmail.com>
2020-07-06 00:28:54 +0800

    course37

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,15 @@ class MyApp extends StatelessWidget {
                 onPressed: answerQuestion,
               ),
               RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: answerQuestion,
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2 chosen!'),
               ),
               RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: answerQuestion,
+                child: Text('Answer 3'),
+                onPressed: () {
+                  //....
+                  print('Answer 3 chosen!');
+                },
               ),
             ],
           )),
