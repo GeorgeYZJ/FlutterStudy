434461a5d7e78ffa3655f164ce86944322b7ac6e
Marketing Intern <marketingintern@stcatsadm.local>
2020-07-06 17:36:21 +0800

    course50

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 //void main() {
 //  runApp(MyApp());
@@ -18,54 +18,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
-      {
-        'questionText': 'What\'s your favourite color?',
-        'answers': ['Balck', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Rabbit', 'snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'Who\'s  your favorite instructor?',
-        'answers': ['Max', 'snake', 'Max', 'Lion'],
-      },
-    ];
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Balck', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Rabbit', 'snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s  your favorite instructor?',
+      'answers': ['Max', 'snake', 'Max', 'Lion'],
+    },
+  ];
   var _questionIndex = 0;
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print('Anwser ${_questionIndex} chosen!');
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
-    } else { 
+    } else {
       print('No more question!');
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: _questionIndex < questions.length ? Column(
-            children: [
-              Question(
-                questions[_questionIndex]['questionText'],
-              ),
-              ...(questions[_questionIndex]['answers'] as List<String>) //a first level list
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-            ],
-          ) : Center(child: Text('You did it!'),),
-          ),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
     );
   }
 }
